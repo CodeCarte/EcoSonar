e90e4fbbb641dd9f1e31f7546ed873d834e0def2
Victor Martins <victorameno@hotmail.com>
2024-06-10 08:24:51 -0300

    final commit

---
+++ assets/js/scriptPage1.js
@@ -0,0 +1,10 @@
+const buttonPage1 = document.getElementById('saiba-mais')
+
+buttonPage1.addEventListener('click', () => {
+    
+    window.location.href = 'assets/html/OurResponsibility.html'
+
+})
+
+
+
---
+++ assets/js/scriptPage2.js
@@ -0,0 +1,8 @@
+
+const buttonPage2 = document.getElementById('Solucoes')
+
+buttonPage2.addEventListener('click', () => {
+
+    window.location.href = '../html/SolutionsPage.html'
+
+})
